1f2c25cf9b3aba0d2311dfa87e6077eec994ccbc
Rizki <104330887+MRizki28@users.noreply.github.com>
2023-02-16 13:18:03 +0800

    add api getby id

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -4,17 +4,37 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\CuacaModel;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ApiController extends Controller
 {
+    //get all data cuaca
+
     public function index()
     {
         $cuaca = CuacaModel::all();
         return response()->json([
-            'message' => 'Success Tampilkan Data',
+            'message' => 'Succes get all data',
             'data' => $cuaca
-        ],Response::HTTP_OK);
+        ], Response::HTTP_OK);
+    }
+
+    //get data by id to use in react js
+
+    public function show(Request $request, $id)
+    {
+        try {
+            $cuacaId = CuacaModel::findOrfail($id);
+            return response()->json([
+                'message' => 'Succes get data by id',
+                'data' => $cuacaId
+            ],Response::HTTP_OK);
+        } catch (ModelNotFoundException $th) {
+            return response()->json([
+                'message' => 'Data tidak ditemukan'
+            ],Response::HTTP_NOT_FOUND);
+        }
     }
 }
