3728252dfa432c79048fea251bed803bbde504ad
Rizki <104330887+MRizki28@users.noreply.github.com>
2023-02-20 15:30:12 +0800

    create url

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\CMS\CuacaController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Base.template');
-});
-Route::get('/cuaca', function () {
-    return view('Admin.cuaca');
+// Route::get('/cuaca', function () {
+//     return view('Admin.cuaca');
+// });
+
+Route::get('/cms/login', function () {
+    return view('Auth.Login');
+})->name('login');
+Route::get('/cms/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::prefix('auth')->controller(AuthController::class)->group(function() {
+    Route::post('/login', 'login')->name('auth.login');
+    Route::get('/logout', 'logout')->name('logout');
 });
 
-Route::post('/cuaca', [CuacaController::class ,'store'])->name('tambahData.cuaca');
-Route::get('/cuaca', [CuacaController::class ,'index']);
+Route::middleware('auth')->group(function() {
+
+    Route::get('/cuaca', [CuacaController::class ,'index']);
+    Route::post('/cuaca', [CuacaController::class ,'store'])->name('tambahData.cuaca');
+    Route::get('/', function () {
+        return view('Base.template');
+    });
+    
+    
+});
